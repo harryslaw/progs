59231efe3cc3441227851cb48d8d3a55883fc36c
Harry S. Law <harryslaw@pm.me>
2018-10-09 01:42:14 +0100

    Update test.c

--- test.c
+++ test.c
@@ -1,84 +1,103 @@
 #include <getopt.h>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#define VERSION "2018.09.08rc4"
+#define VERSION "20181008rc2"
 #define ARGS 0
 
-int usage(const int code, const char* name, const int test);
+int usage(const int code, const char* name);
+
+char* file_read(const char* path);
 
 int
 main(int argc, char** argv)
 {
-	int test = 1;
+	int verbose = 1;
+	char* read = NULL;
+	
+    --argc;
+	if (argc < ARGS) {
+		perror("arguments");
+		usage(1, argv[0]);
+	} else if (argc > ARGS) {
+		perror("arguments");
+		usage(1, argv[0]);
+	} else if (argc != ARGS) {
+		perror("arguments");
+		usage(1, argv[0]);
+	}
 	
 	int i;
-	for (i = 0; (i - 1) < argc; i++) {
+	for (i = 0; i < argc; i++) {
 		int o;
-		while ((o = getopt(argc, *argv, "hvt")) != -1) {
+		while ((o = getopt(argc, argv, "hVv")) != -1) {
 			switch (o) {
 				case 'h':
-					usage(0, argv[0], 0);
+					usage(0, argv[0]);
 					break;
-				case 'v':
+				case 'V':
 					printf("Version: %s\n", VERSION);
 					return 0;
 					break;
-				case 't':
-					test = 1;
+				case 'v':
+					verbose = 1;
 					break;
 				default:
-					fprintf(stderr, "Unrecognised option %c", optopt);
-					usage(1, argv[0], 0);
+					perror("unrecognised option");
+					usage(1, argv[0]);
 					break;
 			}
 		}
-		if ((argc - 1) < ARGS) {
-			if (strcmp(argv[i], "read") == 0) {
-				printf("Reading '%s'\n", argv[0]);
-			}
+	
+		if (strcmp(argv[i], "read") == 0) {
+    int j;
+    for (j = 0; j <= argc; j++) {
+				read = file_read(argv[i + 1]);
+				printf("File: '%s'\n", argv[i + 1]);
+				printf(read);
+			} 
 		}
+		
 		i++;
 	}
 	
-	if (test) {
-		printf("Testing version: %s\n", VERSION);
-	}
-	
-	if ((argc - 1) < ARGS) {
-		fprintf(stderr, "More aguments required\n");
-		usage(1, argv[0], 0);
-	} else if ((argc - 1) > ARGS) {
-		fprintf(stderr, "Less arguments required\n");
-		usage(1, argv[0], 0);
-	} else if ((argc - 1) != ARGS) {
-		fprintf(stderr, "Wrong number of");
-		usage(1, argv[0], 0);
-	} else {
-		if (test) {
-			printf("Testing for aguments: 1\n");
-			int i;
-			for (i = 0; (i - 1) < ARGS; i++) {
-				printf("\t%s\n", argv[i]);
-			}
-		}
-	}
-		
-	if (test) {
-		printf("Testing usage function: %d\n\t%s", usage(0, argv[0], test));
-	}
-	
+	fflush(stdout);
 	return 0;
 }
 
 int
-usage(const int code, const char* name, const int test)
+usage(const int code, const char* name)
 {
-	if (test) {
-		fprintf(stderr, "Usage:\n");
-		fprintf(stderr, "\t\t$ %s [[-h] [-v] [-t]] [ARGS]\n", name);
+	fprintf(stderr, "Usage:\n");
+	fprintf(stderr, "\t$ %s [[-h] [-V] [-v]] [ARGS]\n", name);
+
+	return code;
+}
+
+int
+file_read(const char* path)
+{
+	FILE* fs;
+	long len;
+	char* buf = NULL;
+	
+	if ((fs = fopen(path, "rb")) == NULL) {
+		perror("file");
 		return 1;
 	}
 	
-	return code;
+	fseek(fs, 0, SEEK_END);
+	len = ftell(fs);
+	rewind(fs);
+	
+	buf = (char* )malloc((len + 1) * sizeof(char));
+	fread(buf, len, 1, fs);
+	buf[len] = '\0';
+	
+	fclose(fs);
+        printf(buf);
+	return 0;
 }
+
